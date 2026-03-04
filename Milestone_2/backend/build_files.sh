8d252f9becca9e812f2f7774efc9780bdf240955
Priyadharshini K <kpriyadharshini151@gmail.com>
2026-03-04 22:05:52 +0530

    Fix migrations for Vercel deployment

--- Milestone_2/backend/build_files.sh
+++ Milestone_2/backend/build_files.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-python manage.py migrate --noinput
+echo "Running migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
