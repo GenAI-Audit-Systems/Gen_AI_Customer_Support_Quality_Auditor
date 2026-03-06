141eefe62d44d4e24204cf926f7d93de3f92f1a8
Priyadharshini K <kpriyadharshini151@gmail.com>
2026-03-06 18:36:21 +0530

    Auto run migrations on startup

--- Milestone_2/backend/django_backend/wsgi.py
+++ Milestone_2/backend/django_backend/wsgi.py
@@ -6,15 +6,16 @@ import os
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_backend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_backend.settings")
 
-# Run migrations automatically on startup (important for serverless)
+# Setup Django
+application = get_wsgi_application()
+
+# Run migrations automatically
 try:
     call_command("migrate", interactive=False)
 except Exception as e:
     print("Migration error:", e)
-
-application = get_wsgi_application()
 
 # Vercel entry point
 app = application
